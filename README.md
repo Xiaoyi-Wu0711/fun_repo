a0846509fa72fc7a9214112225a4fd2ba2d170a3
Xiaoyi WU <xiawu@DTU-FQNJMQGXDJ.local>
2024-09-18 17:54:50 -0400

    fun

---
+++ README.md
@@ -0,0 +1,22 @@
+>📋  A template README.md for code accompanying a Machine Learning paper
+
+# RR
+
+This a fun (not really) tutorial for reproducible research (RR) in transportation.  
+
+## Requirements
+
+To install requirements:
+
+```setup
+pip install -r requirements-all-dependencies.txt
+pip install -r requirements-imports.txt
+
+```
+
+>📋  Describe how to set up the environment, e.g. pip/conda/docker commands, download datasets, etc...
+
+
+## Contributing
+
+>📋  MIT Liscence. 
---
+++ simulator.py
@@ -0,0 +1,22 @@
+from pprint import pprint
+from environment import *
+import argparse
+import numpy as np
+my_seed = 175175175
+random.seed(my_seed)
+np.random.seed(my_seed)
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='ITSC 2024 Reproducibility in Transportation Research Tutorial')
+    parser.add_argument('--run-idm', action='store_true')
+    parser.add_argument('--run-custom', action='store_true')
+    parser.add_argument('--no-render', action='store_true', default=False)
+    args = parser.parse_args()
+
+    pprint(vars(args))
+    print("[INFO] Starting the simulator...")
+
+    game = Environment(args)
+    print("[INFO] Created Environment Object...")
+    game.run()
+    print("[INFO] End of the simulation...")
